a61a3934516c787c1629a362601cf0917f0a52c0
Alex Mickelson <alex.mickelson@snow.edu>
2022-11-09 10:01:36 -0700

    only store creature cards

--- 03-deck-manager/DeckManager/DeckService.cs
+++ 03-deck-manager/DeckManager/DeckService.cs
@@ -5,19 +5,23 @@ public class DeckService
 {
   public static void WriteDeckToFile(string path, IEnumerable<Card> deck)
   {
+    IEnumerable<CreatureCard> creatures = deck
+      .Where(c => c.GetType() == typeof(CreatureCard))
+      .Select(c => c as CreatureCard)!;
+
     using (var writer = new StreamWriter(path))
     using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
     {
-      csv.WriteRecords(deck);
+      csv.WriteRecords(creatures);
     }
   }
 
-  public static IEnumerable<Card> ReadDeckFromFile(string path)
+  public static IEnumerable<CreatureCard> ReadDeckFromFile(string path)
   {
     using (var reader = new StreamReader(path))
     using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
     {
-      var deck = csv.GetRecords<Card>();
+      var deck = csv.GetRecords<CreatureCard>();
       return deck.ToArray();
     }
   }
